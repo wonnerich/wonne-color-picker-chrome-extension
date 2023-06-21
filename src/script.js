be9dadee095e57fed63771987efcc1a65c1733cf
wonnerich <fabian.looman@gmail.com>
2023-06-21 22:57:40 +0200

    All Color Extractor

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
 const colorPickerBtn = document.getElementById("color-picker");
+const getAllBtn = document.getElementById("get-all-colors");
 const colorList = document.querySelector(".all-colors");
+const colorListAll = document.querySelector(".all-website-colors-list");
 const clearAll = document.querySelector(".clear-all");
 const pickedColors = JSON.parse(localStorage.getItem("picked-colors") || "[]");
 const pickedColorsRGB = JSON.parse(
   localStorage.getItem("picked-colors-rgb") || "[]"
 );
+
+//HEX to RGB Calculation
 
 function hexToRGB(h) {
   let r = 0,
@@ -24,11 +28,15 @@ function hexToRGB(h) {
   return "rgb(" + +r + "," + +g + "," + +b + ")";
 }
 
+//Copy Color to Clipboard
+
 const copyColor = (elem) => {
   navigator.clipboard.writeText(elem.dataset.color);
   elem.innerText = "Copied";
   setTimeout(() => (elem.innerText = elem.dataset.color), 1000);
 };
+
+//Show selected color
 
 const showColors = () => {
   if (!pickedColors.length) return;
@@ -62,6 +70,8 @@ const showColors = () => {
 };
 showColors();
 
+//Activate EyeDropper
+
 const activateEyeDropper = () => {
   document.body.style.display = "none";
   setTimeout(async () => {
@@ -87,6 +97,8 @@ const activateEyeDropper = () => {
   }, 10);
 };
 
+//Delete all Colors stored in Local Storage
+
 const clearAllColors = () => {
   pickedColors.length = 0;
   localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
@@ -95,3 +107,38 @@ const clearAllColors = () => {
 
 clearAll.addEventListener("click", clearAllColors);
 colorPickerBtn.addEventListener("click", activateEyeDropper);
+
+//Get all Colors of the Website in an Array
+let colorPalette = () => {
+  // Array zum Speichern der Farben
+  var colors = [];
+
+  // Selektieren aller Elemente auf der Webseite
+  var elements = document.querySelectorAll("*");
+
+  // Iterieren über alle Elemente und Extrahieren der Hintergrundfarbe
+  elements.forEach(function (element) {
+    var bgColor = window.getComputedStyle(element).backgroundColor;
+
+    // Überprüfen, ob die Farbe gültig ist und nicht transparent
+    if (bgColor && bgColor !== "rgba(0, 0, 0, 0)") {
+      // Hinzufügen der Farbe zur Palette
+      colors.push(bgColor);
+    }
+  });
+
+  console.log(colors);
+  if (!colors.length) return;
+  colorListAll.innerHTML = colors
+    .map(
+      (color) =>
+        `<li class="color">
+          <span class="rect" style="background: ${color}; border: 1px solid ${
+          color == "rgb(255, 255, 255)" ? "#ccc" : color
+        }""></span></li>`
+    )
+    .join("");
+  document.querySelector(".all-website-colors").classList.remove("hide");
+};
+
+getAllBtn.addEventListener("click", colorPalette);
